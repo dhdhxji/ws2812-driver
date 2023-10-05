536c9b21179556ef6a6046f4abe1b209ae7d58b1
Oleksandr Kovalchuk <oleksandrkovalchuk1998@gmail.com>
2023-10-06 00:05:58 +0300

    Add rz encoder data_request signal Update rz encoder signal naming

--- src/ws2812-unipolar-rz-encoder.v
+++ src/ws2812-unipolar-rz-encoder.v
@@ -2,8 +2,9 @@ module ws2812_unipolar_rz_encoder (
     input wire databit,
 	input wire clk,
     input wire[1:0] command,
-	output reg cmd_wait,
-	output reg data_output
+	output reg cmd_request,
+    output reg data_request,
+	output reg encoded_output
 );
 
 parameter CLK_FREQ_KHZ      = 10000;
@@ -12,7 +13,6 @@ parameter T_HI_FALSE_NS     = 300;
 parameter T_PERIOD_NS       = 1100;
 parameter T_RESET_NS        = 80000;
 
-// Timing values
 localparam CLK_PERIOD_NS    = (1000 * 1000 * 1000) / (CLK_FREQ_KHZ * 1000);
 localparam T_HI_TRUE_TICKS  = T_HI_TRUE_NS / CLK_PERIOD_NS;
 localparam T_HI_FALSE_TICKS = T_HI_FALSE_NS / CLK_PERIOD_NS;
@@ -25,6 +25,7 @@ localparam CMD_IDLE   = 2'b00;
 localparam CMD_TX     = 2'b01;
 localparam CMD_RESET  = 2'b10;
 
+
 localparam STATE_CMD_FETCH_START        = 3'd0;
 localparam STATE_CMD_FETCH_END          = 3'd1;
 localparam STATE_TX_PREP                = 3'd2;
@@ -33,6 +34,7 @@ localparam STATE_TX_DATA_PREFETCH_START = 3'd4;
 localparam STATE_TX_DATA_PREFETCH_END   = 3'd5;
 localparam STATE_RESET_PREP             = 3'd6;
 localparam STATE_RESET                  = 3'd7;
+
 
 reg[2:0] current_state;
 reg[COUNTER_SIZE-1:0] cycle_counter;
@@ -44,12 +46,12 @@ wire encoded_bit_logic = ((cycle_counter < T_HI_TRUE_TICKS) && (tx_data == 1'b1)
 always @(posedge clk) begin
     case (current_state)
         STATE_CMD_FETCH_START: begin
-            cmd_wait <= 1'b1;
+            cmd_request <= 1'b1;
             current_state <= STATE_CMD_FETCH_END;
         end 
 
         STATE_CMD_FETCH_END: begin
-            cmd_wait <= 1'b0;
+            cmd_request <= 1'b0;
             case (command)
                 CMD_TX: current_state <= STATE_TX_PREP;
                 CMD_RESET: current_state <= STATE_RESET_PREP;
@@ -65,22 +67,22 @@ always @(posedge clk) begin
         end 
 
         STATE_TX: begin
-            data_output <= encoded_bit_logic;
+            encoded_output <= encoded_bit_logic;
             cycle_counter <= cycle_counter + 1;
             if (cycle_counter == (T_PERIOD_TICKS - 4)) current_state <= STATE_TX_DATA_PREFETCH_START;
         end 
 
         STATE_TX_DATA_PREFETCH_START: begin
-            data_output <= encoded_bit_logic;
+            encoded_output <= encoded_bit_logic;
             cycle_counter <= cycle_counter + 1;
-            cmd_wait <= 1'b1;
+            data_request <= 1'b1;
             current_state <= STATE_TX_DATA_PREFETCH_END;
         end 
 
         STATE_TX_DATA_PREFETCH_END: begin
-            data_output <= encoded_bit_logic;
+            encoded_output <= encoded_bit_logic;
             cycle_counter <= cycle_counter + 1;
-            cmd_wait <= 1'b0;
+            data_request <= 1'b0;
 
             if (command == CMD_TX) current_state <= STATE_TX_PREP;
             else current_state <= STATE_CMD_FETCH_START;
