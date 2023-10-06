292006ecf21448c3f42f99bf295a3ab20261e725
Oleksandr Kovalchuk <oleksandrkovalchuk1998@gmail.com>
2023-10-06 19:16:06 +0300

    Add reset cycle to rgb encoder bench

--- bench/test_ws2812-rgb-controller.v
+++ bench/test_ws2812-rgb-controller.v
@@ -2,12 +2,22 @@
 
 module test_ws2812_rgb_controller;
 
+localparam CMD_IDLE   = 2'b00;
+localparam CMD_TX     = 2'b01;
+localparam CMD_RESET  = 2'b10;
+
+localparam STATE_DISPLAY_PREP       = 0;
+localparam STATE_DISPLAY            = 1;
+localparam STATE_DELAY_PREP         = 2;
+localparam STATE_DELAY              = 3;
+
 reg clk;
 wire cmd_req;
 wire data_req;
 reg[1:0] cmd;
 wire data_out;
 
+reg[3:0] current_state;
 
 initial begin
     $dumpfile("test_ws2812_rgb_controller.vcd");
@@ -16,14 +26,10 @@ initial begin
     cmd = 2'b01;
     clk = 0;
 
-    #150000 $finish;
+    #1500000 $finish;
 end
 
 always #50 clk <= !clk;
-
-always @(posedge data_req) begin
-    cmd = 2'b10;
-end
 
 ws2812_rgb_controller rgb_controller(
     clk,
@@ -35,5 +41,34 @@ ws2812_rgb_controller rgb_controller(
     data_req,
     data_out
 );
+
+
+
+always @(clk) begin
+	
+	case (current_state)
+		STATE_DISPLAY_PREP: begin
+			cmd <= CMD_TX;
+			if (cmd_req) current_state <= STATE_DISPLAY;
+		end
+		
+		STATE_DISPLAY: begin 
+			cmd <= CMD_IDLE;
+			if (cmd_req) current_state <= STATE_DELAY_PREP;
+		end
+		
+		STATE_DELAY_PREP: begin 
+			cmd <= CMD_RESET;
+			if (cmd_req) current_state <= STATE_DELAY;
+		end
+		
+		STATE_DELAY: begin 
+			if (cmd_req) current_state <= STATE_DISPLAY_PREP;
+		end
+		
+		default: current_state <= STATE_DISPLAY_PREP;
+	endcase
+
+end
 
 endmodule
